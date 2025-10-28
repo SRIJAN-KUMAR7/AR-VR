abeb9f59bc216bef5a70373faa79a49935b75cbd
SRIJAN-KUMAR7 <srijankumar77777@gmail.com>
2025-10-28 17:17:58 +0530

    rest items

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import HatTryOn from './components/HatTryOn';
+import CalibrationUI from './components/CaliberationUi';
+import ModelPicker from './components/ModelPicker';
+import { useCameraDevices } from './hooks/useCameraDevices';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedModel, setSelectedModel] = useState('/models/hat1.glb');
+  const [verticalOffset, setVerticalOffset] = useState(0.0);
+  const [scaleFactor, setScaleFactor] = useState(1.0);
+
+  // Get video input devices
+  const { devices: cameraDevices, selectedDeviceId, setSelectedDeviceId } = useCameraDevices();
 
   return (
-    <>
-      <div>
-
+    <div className="hat-tryon-container">
+      <HatTryOn 
+        modelPath={selectedModel} 
+        verticalOffset={verticalOffset} 
+        scaleFactor={scaleFactor} 
+        deviceId={selectedDeviceId} 
+      />
+      <div className="controls">
+        {/* Camera selection if multiple cameras are available */}
+        {cameraDevices.length > 0 && (
+          <label>
+            Camera:
+            <select
+              onChange={(e) => setSelectedDeviceId(e.target.value)}
+              value={selectedDeviceId || ''}
+            >
+              {cameraDevices.map((device, idx) => (
+                <option key={device.deviceId} value={device.deviceId}>
+                  {device.label || `Camera ${idx + 1}`}
+                </option>
+              ))}
+            </select>
+          </label>
+        )}
+        <CalibrationUI 
+          verticalOffset={verticalOffset} 
+          setVerticalOffset={setVerticalOffset}
+          scaleFactor={scaleFactor}
+          setScaleFactor={setScaleFactor}
+        />
+        <ModelPicker onSelect={(model) => setSelectedModel(model)} />
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
